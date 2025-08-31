6a8839f20d49cdaba208710dff9fc75d49ef4380
hosseini.milad1400 <milad.cia@gmail.com>
2025-08-31 11:28:08 +0330

    init

--- src/env.jsx
+++ src/env.jsx
@@ -1,10 +1,10 @@
 const env = {
   // siteApi:'http://localhost:6026/api',
-  siteApi: "https://admin.helmagroups.com/api",
+  siteApi: "https://admin.helmagroup.com/api",
   // siteApi: "https://demofahaadmin.dkmehr.com/api",
 
   // siteApiUrl:'http://localhost:6026',
-  siteApiUrl: "https://admin.helmagroups.com",
+  siteApiUrl: "https://admin.helmagroup.com",
   // siteApiUrl: "https://demofahaadmin.dkmehr.com",
   cookieName: "helma-login",
   //cookieName:'panel-login',
